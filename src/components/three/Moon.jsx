7cea92e90bdfcda2e0180b3d135337ae56665c2d
Darshan <darshanparmar200d@gmail.com>
2023-10-08 20:26:26 +0530

    moon texture added

--- src/components/three/Moon.jsx
+++ src/components/three/Moon.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import texture from './utils/moon_texture.jpg';
+import { useTexture } from '@react-three/drei';
 export default function Moon({pos}) {
+  const moonTexture = useTexture(texture);
   return (
     <mesh position={[pos.x,pos.y,pos.z]} castShadow receiveShadow>
             <sphereGeometry args={[0.08, 64, 64]}></sphereGeometry>
-            <meshStandardMaterial color="#ffffff"></meshStandardMaterial>
+            <meshStandardMaterial map={moonTexture}></meshStandardMaterial>
     </mesh>
   )
 }
--- src/components/three/Sun.jsx
+++ src/components/three/Sun.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import texture from './utils/sun.jpeg'
+import { useTexture } from '@react-three/drei'
 
 export default function Sun({ pos }) {
+  // const sunTexture = useTexture(texture);
   return (
     <mesh position={[pos.x, pos.y, pos.z]} castShadow receiveShadow>
       <sphereGeometry args={[0.5, 64, 64]}></sphereGeometry>
